f58e6325240175cedd9a9852f627f948cd563e00
Martinc94 <Martnc94@live.co.ik>
2018-06-13 15:59:55 +0100

    changed depricated code

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/',express.static('WebClient'));
 app.use('/mode',express.static('GameClient'));
 
 app.get('/mode/singleplayer', function(req, res) {
-  var noOfPlayers = req.param('players');
+  var noOfPlayers = req.query.players;
   switch (noOfPlayers) {
     case '2':
       //one cpu
